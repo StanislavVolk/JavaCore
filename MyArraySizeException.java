e3b33140c83d9158eb946d611f3acb4a1e267293
StanislavVolk <stanislav.almaty@gmail.com>
2022-04-14 16:18:40 +0600

    Lesson2

---
+++ MyArraySizeException.java
@@ -0,0 +1,15 @@
+package Lesson2;
+class MyArraySizeException extends Exception
+{
+
+    MyArraySizeException(String msg)
+    {
+       super(msg);
+    }
+
+    public MyArraySizeException()
+    {
+
+    }
+}
+
