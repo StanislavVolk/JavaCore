8dafd697b0da0ade362e1d65e885eb0c3b50a0a4
StanislavVolk <stanislav.almaty@gmail.com>
2022-04-18 11:57:12 +0600

    25.04.22

---
+++ Lesson_5/Main.java
@@ -0,0 +1,15 @@
+package Lesson_5;
+
+import java.io.File;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        File file = new File("src/main/java/Lesson_5/test.txt");
+
+
+    }
+
+    //File file = new File()
+}
