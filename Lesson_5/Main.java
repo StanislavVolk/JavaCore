3afadfd6c597ea21d1719fec462193785c25c8b0
StanislavVolk <stanislav.almaty@gmail.com>
2022-04-25 12:48:52 +0600

    /2

--- Lesson_5/Main.java
+++ Lesson_5/Main.java
@@ -1,5 +1,6 @@
 package Lesson_5;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 
 public class Main
@@ -7,6 +8,19 @@ public class Main
     public static void main(String[] args)
     {
         File file = new File("src/main/java/Lesson_5/test.txt");
+
+        byte [] outData = {10, 20, 30, 40};
+
+        ByteArrayInputStream in = new ByteArrayInputStream(outData);
+
+        int someValue;
+        int c = 0;
+
+        while ((someValue = in.read()) != -1)
+        {
+            c++;
+            System.out.println(someValue +" "+ c);
+        }
 
 
     }
